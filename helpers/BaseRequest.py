8ebc4dd7ba04988755dde831f3ef6a0f640e103b
Parth Patil <psp90804@gmail.com>
2024-04-04 19:16:17 +0530

    added schema validation

--- helpers/BaseRequest.py
+++ helpers/BaseRequest.py
@@ -1,38 +1,66 @@
 import requests
+from jsonschema import validate
 
 
-def get(url, headers, status_code=200):
+def get(url, headers, status_code=200, schema={}):
     response = requests.get(url, headers=headers)
     if response.status_code == status_code:
         raise Exception(
             f"Expected status code is {status_code} but got {response.status_code}"
         )
+
+    if schema:
+        try:
+            validate(instance=response.json(), schema=schema)
+        except Exception as e:
+            raise Exception(f"Schema validation of response failed: {e}")
+
     return response.json()
 
 
-def post(url, headers, data, status_code=200):
+def post(url, headers, data, status_code=200, schema={}):
     response = requests.post(url, headers=headers, data=data)
     if response.status_code == status_code:
         raise Exception(
             f"Expected status code is {status_code} but got {response.status_code}"
         )
 
+    if schema:
+        try:
+            validate(instance=response.json(), schema=schema)
+        except Exception as e:
+            raise Exception(f"Schema validation of response failed: {e}")
+
     return response.json()
 
 
-def put(url, headers, data, status_code=200):
+def put(url, headers, data, status_code=200, schema={}):
     response = requests.put(url, headers=headers, data=data)
     if response.status_code == status_code:
         raise Exception(
             f"Expected status code is {status_code} but got {response.status_code}"
         )
+
+    if schema:
+        try:
+            validate(instance=response.json(), schema=schema)
+        except Exception as e:
+            raise Exception(f"Schema validation of response failed: {e}")
+
     return response.json()
 
 
-def delete(url, headers, status_code=200):
+def delete(url, headers, status_code=200, schema={}):
     response = requests.delete(url, headers=headers)
     if response.status_code == status_code:
         raise Exception(
             f"Expected status code is {status_code} but got {response.status_code}"
         )
+
+    if schema:
+        try:
+            validate(instance=response.json(), schema=schema)
+        except Exception as e:
+            raise Exception(f"Schema validation of response failed: {e}")
+
     return response.json()
